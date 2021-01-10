c398fcfe358189af9670dcccaf3a353dcfd95647
shahenazmonia <shahy.m.93@gmail.com>
2021-01-10 19:47:26 +0200

    fix config links

--- src/layout/config.js
+++ src/layout/config.js
@@ -2,11 +2,9 @@ const config = {};
 let baseUrl;
 config.getBasePublicUrl = () => {
   if (process.env.NODE_ENV === 'development') {
-    //     baseUrl = 'https://t3awni-test.herokuapp.com';
     baseUrl = 'https://t3awni-test.herokuapp.com';
   } else if (process.env.NODE_ENV === 'production') {
-    //     baseUrl = 'https://t3awni.com/';
-    baseUrl = 'https://taawoni.com';
+    baseUrl = 'https://t3awni.com/';
   } else {
     baseUrl = 'http://localhost/';
   }
